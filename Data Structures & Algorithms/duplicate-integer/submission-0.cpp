9671af5e5ec909948048434c2d5ef5eaef32cabf
Sheldon Pratap <163245004+slixz7@users.noreply.github.com>
2026-06-17 00:58:46 -0700

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> seen;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (seen.contains(nums[i])) {
+                return true;
+            }
+            seen.insert(nums[i]);
+        }
+        return false;
+
+    }
+};
